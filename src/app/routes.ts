e4cdfa0e2e17abb9dc1e5c96743264e5dedb3a96
Bouih32 <enamto.bouih@gmail.com>
2025-02-20 09:57:44 +0000

    feat: add home route to publicRoutes for improved navigation

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,4 +1,4 @@
-export const publicRoutes = ["/reset", "/contact", "/about", "/community"];
+export const publicRoutes = ["/", "/reset", "/contact", "/about", "/community"];
 export const authRoutes = ["/login", "/signup"];
 export const apiAuthPrefix = "/api/auth";
 export const redirectUser = "/";
