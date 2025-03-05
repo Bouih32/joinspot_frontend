b0b43d21bc7aea9b921189e3e9fb728d97242340
Bouih32 <enamto.bouih@gmail.com>
2025-03-05 13:48:08 +0000

    fix: update API_URL in constantes.ts for local development and adjust login action to use full API_URL

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,11 +1,12 @@
 import axiosInstance from "@/libs/axios";
 import { API_URL } from "@/libs/constantes";
 import { LoginType } from "@/libs/types";
-import { isAxiosError } from "axios";
+import axios, { isAxiosError } from "axios";
 
 export const login = async (data: LoginType) => {
   try {
-    const res = await axiosInstance.post(`/user/login`, data);
+    const res = await axiosInstance.post(`${API_URL}/user/login`, data);
+
     console.log("Response headers:", res.headers);
 
     return res;
