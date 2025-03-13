fb8944c17094161c8431c050b78a6159fa513391
Bouih32 <enamto.bouih@gmail.com>
2025-03-13 11:57:40 +0000

    feat: update UniqueCard styles for improved responsiveness and readability

--- src/components/sections/about/UniqueCard.tsx
+++ src/components/sections/about/UniqueCard.tsx
@@ -10,7 +10,7 @@ export default function UniqueCard({ title, para, value }: UniqueCardProps) {
   return (
     <div
       className={cn(
-        "space-y-2 rounded-[8px] bg-white px-5 py-[30px] tablet:space-y-5 tablet:rounded-xl laptop:p-[30px]",
+        "space-y-2 rounded-[8px] bg-white px-5 py-[30px] tablet:space-y-5 tablet:rounded-xl tablet:px-[15px] laptop:p-[30px]",
         value &&
           "p-5 tablet:space-y-2.5 tablet:p-5 laptop:space-y-5 laptop:p-5",
       )}
@@ -25,8 +25,8 @@ export default function UniqueCard({ title, para, value }: UniqueCardProps) {
       </h4>
       <p
         className={cn(
-          "w-[278px] text-wrap text-16lg tablet:w-[319px]",
-          value && "w-[243px] tablet:w-[243px]",
+          "w-[278px] text-12lg tablet:w-[240px] tablet:text-14lg laptop:w-[319px] laptop:text-16lg",
+          value && "w-[243px] tablet:w-[243px] laptop:w-[243px]",
         )}
       >
         {para}
