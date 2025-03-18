9c8dc55efaf9e01afe4dc3c19f354e924bc84639
Bouih32 <enamto.bouih@gmail.com>
2025-03-18 23:25:38 +0000

    feat: simplify addActivity function by removing commented-out token retrieval and authorization headers

--- src/actions/activityActions.ts
+++ src/actions/activityActions.ts
@@ -1,34 +1,26 @@
 import { API_URL } from "@/libs/constantes";
 import { AddActivityT } from "@/libs/types";
-import Cookies from "js-cookie";
 
 export const addActivity = async (activity: AddActivityT) => {
-  // On the client side
-  // const token = Cookies.get("token"); // Retrieve the token from cookies
-  // console.log(token);
-  // if (!token) return null;
-
   try {
     const res = await fetch(`${API_URL}/activity/add`, {
       method: "POST",
-      credentials: "include", // Ensures cookies are sent
-      // headers: {
-      //   "Content-Type": "application/json",
-      //   Authorization: `Bearer ${token}`,
-      // },
+      credentials: "include", // Ensures cookies are sent automatically
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(activity),
     });
 
     if (!res.ok) {
-      const errorText = await res.text(); // Log the error message returned from the server
+      const errorText = await res.text();
       console.error(`Server responded with ${res.status}:`, errorText);
       throw new Error(
         `HTTP error! Status: ${res.status}, Response: ${errorText}`,
       );
     }
-    const data = await res.json();
 
-    return data;
+    return await res.json();
   } catch (error) {
     console.error("add error", error);
     throw error;
