258ed30e69dcc8b3928320b8340c11b7c0205f6b
Bouih32 <enamto.bouih@gmail.com>
2025-03-25 23:38:40 +0000

    feat: enhance seat validation with range checks and transformation to integer

--- src/libs/validation.ts
+++ src/libs/validation.ts
@@ -150,7 +150,16 @@ export const addValidation = z.object({
   endTime: z.string().trim().min(1, { message: "This field is required" }),
   startDay: z.string().trim().min(1, { message: "This field is required" }),
   endDay: z.string().trim().min(1, { message: "This field is required" }),
-  seat: z.string().trim().min(1, { message: "This field is required" }),
+  seat: z
+    .string()
+    .trim()
+    .min(1, { message: "This field is required" })
+    .max(2, { message: "This field is required" })
+    .transform((val) => parseInt(val, 10)) // Transform the string to an integer
+    .refine((val) => !isNaN(val), { message: "Invalid number" }) // Ensure it's a valid number
+    .refine((val) => val > 2 && val < 50, {
+      message: "The number must be greater than 2 and less than 50",
+    }),
   price: z.string().trim().min(1, { message: "This field is required" }),
   location: z.string().trim().min(3, { message: "This field is required" }),
   cityId: z.string().trim().min(3, { message: "This field is required" }),
