a36ae9a6aaedbbba7863ca65b1f4021e69d3f27f
Bouih32 <enamto.bouih@gmail.com>
2025-04-04 10:44:42 +0000

    feat: update getActivities function to utilize token from cookies for improved data retrieval

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,11 +4,14 @@ import Hero from "@/components/sections/hero/Hero";
 import Offers from "@/components/sections/offers/Offers";
 import Services from "@/components/sections/services/Services";
 import { unstable_cache } from "next/cache";
+import { cookies } from "next/headers";
 
 export default async function Home() {
+  const cookiesStore = await cookies();
+  const cookiesToken = cookiesStore.get("token");
   const getCashedActivities = unstable_cache(
     async () => {
-      const data = await getActivities();
+      const data = await getActivities(cookiesToken);
       return data;
     },
     ["landing_activities"],
