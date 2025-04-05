cc46cd77a52161945791d6f7fe0b69374136852b
Bouih32 <enamto.bouih@gmail.com>
2025-04-05 15:13:35 +0000

    feat: implement UserActivities component for displaying user's activities with caching and no activity handling

--- src/components/activities/UserActivities.tsx
+++ src/components/activities/UserActivities.tsx
@@ -2,17 +2,51 @@ import { getToken } from "@/actions/decodeToken";
 import { getUserActivities } from "@/actions/getActivities";
 import { JwtPayload } from "jsonwebtoken";
 import React from "react";
+import ActivityCard from "./ActivityCard";
+import { nanoid } from "nanoid";
+import NoActivities from "./NoActivities";
+import { unstable_cache } from "next/cache";
+import { revalidate } from "@/libs/constantes";
 
-export default async function UserActivities() {
+export default async function UserActivities({
+  params,
+}: {
+  params: {
+    seats: string;
+    category: string;
+    date: string;
+    my: string;
+    search: string;
+  };
+}) {
   const token = await getToken();
   let userId: string | undefined;
-  console.log(token);
 
   if (typeof token !== "string" && token !== null) {
     userId = (token as JwtPayload).userId;
   }
 
   if (!userId) return;
-  const userActivities = await getUserActivities(userId);
-  return <div>UserActivities</div>;
+
+  const cachedUserActivities = unstable_cache(
+    async () => {
+      const userActivities = await getUserActivities(userId);
+      return userActivities;
+    },
+    [userId, "activities"],
+    { tags: [userId, "activities"], revalidate: revalidate },
+  );
+
+  const userActivities = await cachedUserActivities();
+  return (
+    <div className="flex w-full flex-col items-start space-y-4 pb-5 tablet:space-y-5">
+      {!userActivities || userActivities.length === 0 ? (
+        <NoActivities token={token} params={params} />
+      ) : (
+        userActivities.map((ele) => (
+          <ActivityCard key={nanoid()} full data={ele} />
+        ))
+      )}
+    </div>
+  );
 }
