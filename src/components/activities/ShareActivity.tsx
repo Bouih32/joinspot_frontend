3883b0bc44b26fbdfb1543886c9b4e029396f8b6
Bouih32 <enamto.bouih@gmail.com>
2025-04-22 21:22:22 +0000

    feat: implement Portal component for modal rendering in ShareActivity

--- src/components/activities/ShareActivity.tsx
+++ src/components/activities/ShareActivity.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { FaRegCopy, FaRegShareSquare } from "react-icons/fa";
 
 import { MdOutlineIosShare } from "react-icons/md";
+import Portal from "./Portal";
 
 export default function ShareActivity({
   activityId,
@@ -72,65 +73,69 @@ export default function ShareActivity({
       )}
 
       {open && (
-        <div
-          className="fixed inset-0 z-[800] grid cursor-pointer place-content-center bg-white/50"
-          onClick={() => setOpen(false)}
-        >
+        <Portal>
+          {" "}
           <div
-            onClick={(e) => e.stopPropagation()}
-            className="flex w-[259px] cursor-default flex-col gap-4 rounded-[8px] bg-white px-4 py-4 text-14lg text-second shadow-8xl tablet:w-[320px] tablet:rounded-xl tablet:px-5 tablet:py-[30px] tablet:text-16lg"
+            className="fixed inset-0 z-[800] grid cursor-pointer place-content-center bg-white/50"
+            onClick={() => setOpen(false)}
           >
-            {copy && (
-              <div className="w-full rounded border border-success bg-successHover px-5 py-1 text-center text-12xxl text-success tablet:text-12xxl">
-                <p className="text-center">
-                  Link Copied! Share it anywhere and invite others to JoinSpot!
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className="flex w-[259px] cursor-default flex-col gap-4 rounded-[8px] bg-white px-4 py-4 text-14lg text-second shadow-8xl tablet:w-[320px] tablet:rounded-xl tablet:px-5 tablet:py-[30px] tablet:text-16lg"
+            >
+              {copy && (
+                <div className="w-full rounded border border-success bg-successHover px-5 py-1 text-center text-12xxl text-success tablet:text-12xxl">
+                  <p className="text-center">
+                    Link Copied! Share it anywhere and invite others to
+                    JoinSpot!
+                  </p>
+                </div>
+              )}
+              <h3 className="text-16xxl text-main tablet:text-20xxl">
+                Share this post with others!
+              </h3>
+              <div className="space-y-3">
+                <p>
+                  Spread the word! Copy the link below or share it on social
+                  media so others can check out this post.
                 </p>
               </div>
-            )}
-            <h3 className="text-16xxl text-main tablet:text-20xxl">
-              Share this post with others!
-            </h3>
-            <div className="space-y-3">
-              <p>
-                Spread the word! Copy the link below or share it on social media
-                so others can check out this post.
-              </p>
-            </div>
 
-            <div className="flexBetween gap-3 rounded border border-second px-3 py-[6px]">
-              <p className="line-clamp-1 w-full overflow-hidden text-14lg text-main">
-                {link}
-              </p>
-              <FaRegCopy
-                className="cursor-pointer hover:text-main"
-                onClick={handleCopy}
-              />
-            </div>
-
-            <section className="grid grid-cols-2 gap-2">
-              <div
-                onClick={handleFacebookShare}
-                className="flexCenter cursor-pointer rounded bg-[#2463EB] px-3 py-[6px] text-14xl text-white"
-              >
-                <p> Facebook</p>
+              <div className="flexBetween gap-3 rounded border border-second px-3 py-[6px]">
+                <p className="line-clamp-1 w-full overflow-hidden text-14lg text-main">
+                  {link}
+                </p>
+                <FaRegCopy
+                  className="cursor-pointer hover:text-main"
+                  onClick={handleCopy}
+                />
               </div>
-              <div
-                onClick={handleWhatsAppShare}
-                className="flexCenter cursor-pointer rounded bg-[#22C55D] px-3 py-[6px] text-14xl text-white"
-              >
-                <p> Whatsapp</p>
-              </div>
-            </section>
 
-            <div
-              onClick={(e) => {
-                setOpen(false);
-              }}
-            >
-              <Button classname="w-full">Cancel</Button>
+              <section className="grid grid-cols-2 gap-2">
+                <div
+                  onClick={handleFacebookShare}
+                  className="flexCenter cursor-pointer rounded bg-[#2463EB] px-3 py-[6px] text-14xl text-white"
+                >
+                  <p> Facebook</p>
+                </div>
+                <div
+                  onClick={handleWhatsAppShare}
+                  className="flexCenter cursor-pointer rounded bg-[#22C55D] px-3 py-[6px] text-14xl text-white"
+                >
+                  <p> Whatsapp</p>
+                </div>
+              </section>
+
+              <div
+                onClick={(e) => {
+                  setOpen(false);
+                }}
+              >
+                <Button classname="w-full">Cancel</Button>
+              </div>
             </div>
           </div>
-        </div>
+        </Portal>
       )}
     </div>
   );
--- src/components/activities/UserCard.tsx
+++ src/components/activities/UserCard.tsx
@@ -1,6 +1,4 @@
 import Image from "next/image";
-
-import { useRouter } from "next/navigation";
 import { avatarPlaceholder } from "@/libs/constantes";
 import ShareActivity from "./ShareActivity";
 import Link from "next/link";
@@ -22,8 +20,6 @@ export default function UserCard({
   tokenId,
   activityId,
 }: UserCardProps) {
-  const router = useRouter();
-
   return (
     <div
       className="flex items-start justify-between"
