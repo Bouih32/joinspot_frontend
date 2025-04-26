9fc47a873d1d2258a9238e6982fec5ffa47c740b
Bouih32 <enamto.bouih@gmail.com>
2025-04-26 19:52:53 +0000

    fix: update getPosts function to return data.posts instead of data.data

--- src/actions/postServer.ts
+++ src/actions/postServer.ts
@@ -31,7 +31,7 @@ export const getPosts = async (params?: Record<string, string>) => {
 
     const data = await res.json();
 
-    return data.posts;
+    return data.data;
   } catch (error) {
     console.error("Error fetching activities:", error);
     throw error;
