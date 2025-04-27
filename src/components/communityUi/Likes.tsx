3965355b8297cde64deb0154417eaa555bb46911
Bouih32 <enamto.bouih@gmail.com>
2025-04-27 01:00:56 +0000

    feat: implement createTicket function for ticket creation; update JoinForm to handle ticket creation after payment fix: update Likes component to manage loading state and button disable during like action

--- src/components/communityUi/Likes.tsx
+++ src/components/communityUi/Likes.tsx
@@ -10,17 +10,23 @@ type LikesProps = {
 
 export default function Likes({ likes, postId }: LikesProps) {
   const [like, setLike] = useState(false);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const handleLike = async () => {
-    await likePost(postId);
     setLike((prev) => !prev);
+    setLoading(true);
+    await likePost(postId);
+
+    setLoading(true);
     router.refresh();
   };
   return (
     <div className="flex items-center gap-[6px]">
       <div onClick={handleLike} className="cursor-pointer">
-        {like ? <FaHeart className="text-main" /> : <FaRegHeart />}
+        <button disabled={loading}>
+          {like ? <FaHeart className="text-main" /> : <FaRegHeart />}
+        </button>
       </div>
 
       <p>{likes} Likes</p>
